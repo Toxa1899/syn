58f8590291fb1899338937d553358957ecf2a200
Toxa1899 <ralz9-ralz9@mail.ru>
2024-10-13 17:34:34 +0600

    add swagger

--- config/urls.py
+++ config/urls.py
@@ -2,12 +2,22 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API",
+        default_version="v1",
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/user/", include("applications.account.urls")),
     path("api/v1/products", include("applications.account.urls")),
+    path("swagger/", schema_view.with_ui("swagger")),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
